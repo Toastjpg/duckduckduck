4ba0390f4c061e2ba350c4f5f7540300154636d2
Johnny <52947117+Toastjpg@users.noreply.github.com>
2022-04-30 11:51:50 -0700

    added sound effects to the ducks

--- app/src/main/java/mountainmadness/duckduckduck/app/MainActivity.java
+++ app/src/main/java/mountainmadness/duckduckduck/app/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.res.TypedArray;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,11 +17,16 @@ import java.util.Random;
 import mountainmadness.duckduckduck.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String DEFAULT_TITLE_TEXT = "Tap the duck to duck the duck!";
     private TypedArray defaultImg;
     private TypedArray altImg;
     private ImageView image;
     private TextView text;
     private Random rand;
+    private Handler handler;
+    private MediaPlayer mpduck;
+    private MediaPlayer mpalt;
+    private boolean clickable = true;
     private int randomIndex;
     private int primary;
     private int alt;
@@ -31,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
         defaultImg = getResources().obtainTypedArray(R.array.defaultimg);
         altImg = getResources().obtainTypedArray(R.array.altimg);
+        mpduck = MediaPlayer.create(this, R.raw.quack_sound);
+        mpalt = MediaPlayer.create(this, R.raw.cow_sound);
+
+        handler = new Handler();
 
         rand = new Random();
         randomIndex = rand.nextInt(defaultImg.length());
@@ -39,14 +50,9 @@ public class MainActivity extends AppCompatActivity {
         image = findViewById(R.id.main_image);
         text = findViewById(R.id.title_text);
         image.setImageResource(primary);
-        text.setText("Tap the duck to duck the duck!");
+        text.setText(DEFAULT_TITLE_TEXT);
         setUpImage();
         setUpRefreshButton();
-    }
-
-    private void setImages(int index){
-        primary = defaultImg.getResourceId(index, 0);
-        alt = altImg.getResourceId(index, 0);
     }
 
     private void setUpRefreshButton() {
@@ -59,26 +65,51 @@ public class MainActivity extends AppCompatActivity {
             randomIndex = temp;
 
             setImages(randomIndex);
-            text.setText("Tap the duck to duck the duck!");
+            text.setText(DEFAULT_TITLE_TEXT);
             image.setImageResource(primary);
         });
     }
 
     @SuppressLint("ClickableViewAccessibility")
     private void setUpImage(){
-        image.setOnTouchListener((v, event) -> {
-            if(event.getAction() == MotionEvent.ACTION_DOWN){
-                // set alt image here
-                text.setText("Duck!");
+        image.setOnClickListener(view -> {
+            if (clickable){
+                // set alt image and play sound clip here
+                clickable = false;
                 image.setImageResource(alt);
-            }
-            if(event.getAction() == MotionEvent.ACTION_UP){
-                // Set default img here
-                text.setText("Not Ducked!");
-                image.setImageResource(primary);
-            }
-            return true;
-        });
+                int random = rand.nextInt(10);
 
+                if (random <= 2) {
+                    text.setText("Duck??");
+                    playSound(false);
+                }
+                else{
+                    text.setText("Duck!!");
+                    playSound(true);
+                }
+
+                handler.postDelayed(() -> {
+                    // Do something after 5s = 5000ms
+                    // Set default img here
+                    text.setText(DEFAULT_TITLE_TEXT);
+                    image.setImageResource(primary);
+                    clickable = true;
+                }, 1500);
+            }
+
+        });
+    }
+
+    private void playSound(boolean isDuck) {
+        if ((isDuck)) {
+            mpduck.start();
+        } else {
+            mpalt.start();
+        }
+    }
+
+    private void setImages(int index){
+        primary = defaultImg.getResourceId(index, 0);
+        alt = altImg.getResourceId(index, 0);
     }
 }
